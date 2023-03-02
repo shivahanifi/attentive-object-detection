ba7c4af7b8066e3b217ab177b5393a6be7fcdfd4
icub <icub@iit.it>
2023-03-02 17:22:02 +0000

    aod.py debugging

--- src/aod.py
+++ src/aod.py
@@ -8,6 +8,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+from PIL import Image
 from config_vt import *
 
 
@@ -114,6 +115,7 @@ class AttentiveObjectDetection(yarp.RFModule):
     
     # IoU
     def iou(boxA, boxB):
+        print("unexpected argument received" + str(err))
         # determine the (x, y)-coordinates of the intersection rectangle
         xA = max(boxA[0], boxB[0])
         yA = max(boxA[1], boxB[1])
@@ -136,15 +138,24 @@ class AttentiveObjectDetection(yarp.RFModule):
     def updateModule(self):
 
         # Recieve the inpu image
-        frame_raw = self.in_port_scene_image.read()
-        if frame_raw is not None:
-            self.in_buf_scene_image.copy(frame_raw)
-            self.out_buf_propag_image.copy(frame_raw)
+        received_image = self.in_port_scene_image.read()
+        if received_image is not None:
+            self.in_buf_scene_image.copy(received_image)
+            self.out_buf_propag_image.copy(received_image)
             assert self.in_buf_scene_array.__array_interface__['data'][0] == self.in_buf_scene_image.getRawImage().__int__()   
+        
+        frame_raw = Image.fromarray(self.in_buf_scene_array) 
 
 
         # Recieve heatmap bbox data
+        hm_bbox_data = yarp.Bottle()
+        hm_bbox_data.clear()
         hm_bbox_data = self.in_port_hm_bbox_data.read()
+        hm_bbox_data_list = []
+        for i in range(hm_bbox_data.size()):
+             hm_bbox_data_list.append(hm_bbox_data.get(i).asFloat32())
+        print("hm_bbox_data_list is: ", hm_bbox_data_list)
+
 
         # Recieve object detection data
         obj_det_data = yarp.Bottle()
@@ -154,9 +165,9 @@ class AttentiveObjectDetection(yarp.RFModule):
         if obj_det_data is not None:  
             for i in range(0, obj_det_data.size()):  
                 dets = obj_det_data.get(i).asList() 
-                if dets.get(0).isDouble():  
-                    bbox = [dets.get(0).asDouble(), dets.get(1).asDouble(), dets.get(2).asDouble(),  
-                            dets.get(3).asDouble()] # bbox format: [tl_x, tl_y, br_x, br_y]
+                if dets.get(0).isFloat64():  
+                    bbox = [dets.get(0).asFloat64(), dets.get(1).asFloat64(), dets.get(2).asFloat64(),  
+                            dets.get(3).asFloat64()] # bbox format: [tl_x, tl_y, br_x, br_y]
                     cls = dets.get(5).asString() # label of i-th detection
 
                     detection_dict = { 
@@ -166,12 +177,16 @@ class AttentiveObjectDetection(yarp.RFModule):
 
         # Selection & Visualization       
         max_iou = 0
-        wallpaper = frame_raw
+        wallpaper = np.asarray(frame_raw)
         for pred in predictions:
             obj_det_bbox = detection_dict.get("bbox")
-            bbox_draw = cv2.rectangle(wallpaper, obj_det_bbox[0:2], obj_det_bbox[2:4], (0, 0, 255), 3)
-            wallpaper = bbox_draw            
-            iou_value = self.iou(hm_bbox_data, obj_det_bbox)
+            print("obj_det_bbox is:", obj_det_bbox)
+            obj_det_bbox_array = np.array(obj_det_bbox, dtype=np.float64)
+            obj_det_bbox_float32 = obj_det_bbox_array.astype(np.float32).tolist()
+            bbox_draw = cv2.rectangle(wallpaper, (int(obj_det_bbox[0]), int(obj_det_bbox[1])), (int(obj_det_bbox[2]), int(obj_det_bbox[3])), (0, 0, 255), 3)
+            wallpaper = bbox_draw 
+            print("inputs of iou: ", hm_bbox_data_list,obj_det_bbox_float32)         
+            iou_value = self.iou(hm_bbox_data_list,obj_det_bbox_float32)
             if iou_value > max_iou:
                 selected_obj_label = detection_dict.get("class")
                 selected_obj_bbox = obj_det_bbox

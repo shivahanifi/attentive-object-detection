d6cce283f62c3497b18185d0a4728b198dbb9cbf
shivahanifi <shiva.hnf@gmail.com>
2024-04-03 10:37:26 +0200

    src files updated

--- src/aod.py
+++ src/aod.py
@@ -73,6 +73,10 @@ class AttentiveObjectDetection(yarp.RFModule):
                                              self.out_buf_propag_image_array.shape[0])
         print('{:s} opened'.format('/aod/propag:o'))
 
+        self.no_hm_bbox_count = 0
+        self.old_predictions = []
+        self.obj_det_memory = None
+
         return True
     
     # Respond to a message
@@ -142,9 +146,8 @@ class AttentiveObjectDetection(yarp.RFModule):
         if len(p) != len(q):
             raise ValueError("Points must have the same number of dimensions")
         return math.sqrt(sum((p[i] - q[i]) ** 2 for i in range(len(p))))
-
+    
     def updateModule(self):
-
         # Recieve the inpu image
         received_image = self.in_port_scene_image.read()
         if received_image is not None:
@@ -169,11 +172,16 @@ class AttentiveObjectDetection(yarp.RFModule):
                             'bbox': bbox,  
                             'class': cls  } 
                         predictions.append(detection_dict)
+                if len(predictions) == 0:
+                   predictions = list(self.old_predictions)
+                else:
+                   self.old_predictions = list(predictions)
             
                 # Recieve heatmap bbox data
                 hm_bbox_data = yarp.Bottle()
                 hm_bbox_data.clear()
                 hm_bbox_data = self.in_port_hm_bbox_data.read(False)
+                self.hm_bbox_data_memory = hm_bbox_data
                 hm_bbox_data_list = []
 
                 if hm_bbox_data is not None:
@@ -190,6 +198,7 @@ class AttentiveObjectDetection(yarp.RFModule):
                         top_left = (int(obj_det_bbox[0]), int(obj_det_bbox[1]))
                         bottom_right = (int(obj_det_bbox[2]), int(obj_det_bbox[3]))
                         label_draw = cv2.putText(wallpaper, obj_label, (top_left[0], top_left[1]-20), 
+
                                                 cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 1, 2)
                         bbox_draw = cv2.rectangle(label_draw, top_left, bottom_right, (0, 0, 255), 2)
                         wallpaper = bbox_draw         
@@ -222,20 +231,41 @@ class AttentiveObjectDetection(yarp.RFModule):
                         self.out_buf_detection_array[:, :] = selected_obj
                         self.out_port_detection_image.write( self.out_buf_detection_image)
                     else:
-                        min_distance = 1000000
+                        print('here')
+                        min_distance = 10000000
                         center_hm_bbox = ((hm_bbox_data_list[0] + hm_bbox_data_list[2])/2 , 
-                                        (hm_bbox_data_list[1] + hm_bbox_data_list[3])/2)    
-                        for pred in predictions:
-                            obj_label = pred.get("class")
-                            obj_det_bbox = pred.get("bbox")
-                            obj_det_bbox_float32 = np.array(obj_det_bbox, dtype=np.float32).tolist()
-                            center_obj_det_bbox = ((obj_det_bbox_float32[0] + obj_det_bbox_float32[2])/2 , 
+                                        (hm_bbox_data_list[1] + hm_bbox_data_list[3])/2)
+                        if True:
+                            self.old_predictions = predictions
+                            for pred in predictions:
+                                print('Inside predictions')
+                                obj_label = pred.get("class")
+                                obj_det_bbox = pred.get("bbox")
+                                obj_det_bbox_float32 = np.array(obj_det_bbox, dtype=np.float32).tolist()
+                                print(obj_det_bbox_float32)
+                                if len(obj_det_bbox_float32):
+                                    center_obj_det_bbox = ((obj_det_bbox_float32[0] + obj_det_bbox_float32[2])/2 , 
                                                     (obj_det_bbox_float32[1] + obj_det_bbox_float32[3])/2)
-                            distance = self.dist(center_hm_bbox, center_obj_det_bbox)
-                            if distance < min_distance:
-                                min_distance = distance
-                                selected_obj_label = obj_label
-                                selected_obj_bbox = obj_det_bbox_float32  
+                                    distance = self.dist(center_hm_bbox, center_obj_det_bbox)
+                                    print('detected distance is: ',distance)
+                                    
+                                    if distance < min_distance:
+                                        min_distance = distance
+                                        selected_obj_label = obj_label
+                                        selected_obj_bbox = obj_det_bbox_float32
+
+#                        else:
+#                            print('No object detected, trying the memory object')
+#                            predictions = self.predictions
+#                            obj_det_bbox_float32 = np.array(obj_det_bbox, dtype=np.float32).tolist()
+#                            center_obj_det_bbox = ((obj_det_bbox_float32[0] + obj_det_bbox_float32[2])/2 ,
+#                                                    (obj_det_bbox_float32[1] + obj_det_bbox_float32[3])/2)
+#                            distance = self.dist(center_hm_bbox, center_obj_det_bbox)
+#                            print('ditance is: ', distance)
+#                            if distance < min_distance:
+#                                min_distance = distance
+#                                selected_obj_label = obj_label
+#                                selected_obj_bbox = obj_det_bbox_float32  
                         print("The visually attended object selected by DISTANCE is", selected_obj_label)                  
                         selected_label = cv2.putText(wallpaper, selected_obj_label, (int(selected_obj_bbox[0]),
                                                     int(selected_obj_bbox[1])-20), cv2.FONT_HERSHEY_SIMPLEX, 
@@ -258,23 +288,120 @@ class AttentiveObjectDetection(yarp.RFModule):
                         # Output to yarp port
                         self.out_buf_detection_array[:, :] = selected_obj
                         self.out_port_detection_image.write( self.out_buf_detection_image)
+                        buffered_output = self.out_buf_detection_image
+
 
                         # Copy output to propagation port
                         self.out_buf_propag_image_array = selected_obj
                         self.out_port_propag_image.write(self.out_buf_propag_image)
                 else:
-                    # Output to yarp port
-                    print('No heatmap detected')
-                    no_object = cv2.putText(np.asarray(frame_raw), 'Non of the objects visually attended.', (30,30), cv2.FONT_HERSHEY_SIMPLEX, 
-                                                 0.7, (255, 0, 0), 2, 2)
-                    no_object_array = np.asarray(no_object)
-                    self.out_buf_detection_array[:, :] = no_object_array
-                    self.out_port_detection_image.write( self.out_buf_detection_image)
+                    if False:
+                        # Output to yarp port
+                        print('No heatmap detected, providing the previous detection')
+                        
+                        hm_bbox_data = self.hm_bbox_data_memory
+                        hm_bbox_data_list = []
+                        for i in range(hm_bbox_data.size()):
+                            hm_bbox_data_list.append(hm_bbox_data.get(i).asFloat32())
+                        # Selection & Visualization       
+                        max_iou = 0
+                        selected_obj_label = None
+                        wallpaper = np.asarray(frame_raw)
+                        for pred in predictions:
+                            obj_label = pred.get("class")
+                            obj_det_bbox = pred.get("bbox")
+                            obj_det_bbox_float32 = np.array(obj_det_bbox, dtype=np.float32).tolist()
+                            top_left = (int(obj_det_bbox[0]), int(obj_det_bbox[1]))
+                            bottom_right = (int(obj_det_bbox[2]), int(obj_det_bbox[3]))
+                            label_draw = cv2.putText(wallpaper, obj_label, (top_left[0], top_left[1]-20),
 
-                    # Output to yarp port- selected bbox data
-                    selected_bbox_data = yarp.Bottle() 
-                    selected_bbox_data.addString('None of the objects visually attended.')
-                    self.out_port_bbox_data.write(selected_bbox_data)
+                                                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 1, 2)
+                            bbox_draw = cv2.rectangle(label_draw, top_left, bottom_right, (0, 0, 255), 2)
+                            wallpaper = bbox_draw
+                            iou_value = self.iou(hm_bbox_data_list,obj_det_bbox_float32)
+                            if iou_value > max_iou:
+                                selected_obj_label = obj_label
+                                selected_obj_bbox = obj_det_bbox
+                                max_iou = iou_value
+
+                        if selected_obj_label is not None:
+                            print("The visually attended object selected by IoU is", selected_obj_label)
+                            selected_label = cv2.putText(wallpaper, selected_obj_label, (int(selected_obj_bbox[0]),
+                                                    int(selected_obj_bbox[1])-20), cv2.FONT_HERSHEY_SIMPLEX,
+                                                    0.7, (0, 255, 0), 2, 2)
+                            selected_obj = cv2.rectangle(selected_label, (int(selected_obj_bbox[0]),
+                                                    int(selected_obj_bbox[1])), (int(selected_obj_bbox[2]),
+                                                    int(selected_obj_bbox[3])), (0, 255, 0), 3)
+
+                            # Output to yarp port- selected bbox data
+                            selected_bbox_data = yarp.Bottle()
+                            selected_bbox_data.addFloat32(selected_obj_bbox[0])
+                            selected_bbox_data.addFloat32(selected_obj_bbox[1])
+                            selected_bbox_data.addFloat32(selected_obj_bbox[2])
+                            selected_bbox_data.addFloat32(selected_obj_bbox[3])
+                            selected_bbox_data.addString(selected_obj_label)
+                            self.out_port_bbox_data.write(selected_bbox_data)
+
+                            # Output to yarp port- Image
+                            self.out_buf_detection_array[:, :] = selected_obj
+                            self.out_port_detection_image.write( self.out_buf_detection_image)
+                        else:
+                            min_distance = 1000000000000000000
+                            center_hm_bbox = ((hm_bbox_data_list[0] + hm_bbox_data_list[2])/2 ,
+                                        (hm_bbox_data_list[1] + hm_bbox_data_list[3])/2)
+                            for pred in predictions:
+                                obj_label = pred.get("class")
+                                obj_det_bbox = pred.get("bbox")
+                                obj_det_bbox_float32 = np.array(obj_det_bbox, dtype=np.float32).tolist()
+                                center_obj_det_bbox = ((obj_det_bbox_float32[0] + obj_det_bbox_float32[2])/2 ,
+                                                    (obj_det_bbox_float32[1] + obj_det_bbox_float32[3])/2)
+                            distance = self.dist(center_hm_bbox, center_obj_det_bbox)
+                            if distance < min_distance:
+                                min_distance = distance
+                                selected_obj_label = obj_label
+                                selected_obj_bbox = obj_det_bbox_float32
+                            print("The visually attended object selected by DISTANCE is", selected_obj_label)
+                            selected_label = cv2.putText(wallpaper, selected_obj_label, (int(selected_obj_bbox[0]),
+                                                    int(selected_obj_bbox[1])-20), cv2.FONT_HERSHEY_SIMPLEX,
+                                                    0.7, (0, 255, 0), 2, 2)
+                            selected_obj = cv2.rectangle(selected_label, (int(selected_obj_bbox[0]),
+                                                    int(selected_obj_bbox[1])), (int(selected_obj_bbox[2]),
+                                                    int(selected_obj_bbox[3])), (0, 255, 0), 5)
+
+
+                            # Output to yarp port- selected bbox data
+                            selected_bbox_data = yarp.Bottle()
+                            selected_bbox_data.addFloat32(selected_obj_bbox[0])
+                            selected_bbox_data.addFloat32(selected_obj_bbox[1])
+                            selected_bbox_data.addFloat32(selected_obj_bbox[2])
+                            selected_bbox_data.addFloat32(selected_obj_bbox[3])
+                            selected_bbox_data.addString(selected_obj_label)
+                            self.out_port_bbox_data.write(selected_bbox_data)
+
+
+                            # Output to yarp port
+                            self.out_buf_detection_array[:, :] = selected_obj
+                            self.out_port_detection_image.write( self.out_buf_detection_image)
+                            buffered_output = self.out_buf_detection_image
+
+
+                            # Copy output to propagation port
+                            self.out_buf_propag_image_array = selected_obj
+                            self.out_port_propag_image.write(self.out_buf_propag_image)
+
+                        self.no_heatmap_count +=1
+                    else:
+                        print('No heatmap detected for more than 3 frames')
+                        no_object = cv2.putText(np.asarray(frame_raw), 'Non of the objects visually attended.', (30,30), cv2.FONT_HERSHEY_SIMPLEX, 
+                                                 0.7, (255, 0, 0), 2, 2)
+                        no_object_array = np.asarray(no_object)
+                        self.out_buf_detection_array[:, :] = no_object_array
+                        self.out_port_detection_image.write( self.out_buf_detection_image)
+
+                        # Output to yarp port- selected bbox data
+                        selected_bbox_data = yarp.Bottle() 
+                        selected_bbox_data.addString('None of the objects visually attended.')
+                        self.out_port_bbox_data.write(selected_bbox_data)
             else:
                 print("No information recieved from object detection module")
         else:
